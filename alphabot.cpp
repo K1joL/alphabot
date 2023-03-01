0c8f1c7d76059756c31c2cbba92dc9e3cfa1e696
K1joL <100405888+K1joL@users.noreply.github.com>
2023-03-01 22:21:01 +0300

    UPD3
    Added build folder
    Added implementation of all methods and functions.
    Method "detectBlob" was split in two parts: "detectBlob" and "TakeThresholdOfBlob"
    Added new Function "SetColor" just for use on computer. Its helper method to choose optimal color range to detect.
    Added main function implementation.
    Code refactoring.

--- alphabot.cpp
+++ alphabot.cpp
@@ -6,10 +6,17 @@ using namespace cv;
 
 struct Color
 {
-    int Red;
-    int Green;
-    int Blue;
-};
+    uint8_t Hue = 0;
+    uint8_t Saturation = 0;
+    uint8_t Value = 0;
+    Color(uint8_t H, uint8_t S, uint8_t V)
+    {
+        Hue = H;
+        Saturation = S;
+        Value = V;
+    }
+
+}BlueHSV(100,90,70), MagentaHSV(167, 80, 90);
 
 class MassCenter
 {
@@ -25,18 +32,10 @@ class MovementCalculation
     public:
         float AngleInRadian;
         int DistanceInPixel;
-
         void findAngle(MassCenter blob1, MassCenter blob2, Point2i Destination);
         void findDistanceToDestination(MassCenter AverageCenter, Point2i Destination);
 
 };
-
-int main()
-{
-    cout << "Hello Robot!" << endl;
-
-    return 0;
-}
 
 MassCenter::MassCenter(Rect Rectangle)
 {
@@ -65,7 +64,110 @@ void MovementCalculation::findDistanceToDestination(MassCenter AverageCenter, Po
     this -> DistanceInPixel = (AverageCenter.X - Dest.x) - (AverageCenter.Y - Dest.y);
 }
 
-Rect detectBlob(Mat Frame, Color c)
+void SetColor(Mat Frame)
 {
+    namedWindow("Result");
+    namedWindow("Settings");
+    createTrackbar("h1", "Settings", 0, 255);
+    createTrackbar("s1", "Settings", 0, 255);
+    createTrackbar("v1", "Settings", 0, 255);
+    createTrackbar("h2", "Settings", 0, 255);
+    createTrackbar("s2", "Settings", 0, 255);
+    createTrackbar("v2", "Settings", 0, 255);
+    while(true)
+    {
+        Mat FrameHsv;
+        cvtColor(Frame, FrameHsv, COLOR_BGR2HSV);
+        int h1 = getTrackbarPos("h1", "Settings");
+        int s1 = getTrackbarPos("s1", "Settings");
+        int v1 = getTrackbarPos("v1", "Settings");
+        int h2 = getTrackbarPos("h2", "Settings");
+        int s2 = getTrackbarPos("s2", "Settings");
+        int v2 = getTrackbarPos("v2", "Settings");
+        vector <int> HsvMin = {h1, s1, v1};
+        vector <int> HsvMax = {h2, s2, v2};
+        Mat thresh;
+        inRange(FrameHsv, HsvMin, HsvMax, thresh);
+        imshow("Result", thresh);
+
+        char ch = waitKey(1000);
+        if (ch == 27)
+            break;
+    }
+    destroyAllWindows;
+}
+
+Mat TakeThresholdOfBlob(Mat FrameHSV, Color Color)
+{
+    Mat Threshold;
+    //vectors of HSV color that need to find. Make a range using Offset
+    uint8_t Offset = 5;
+    vector <uint8_t> HsvMin = {static_cast<uint8_t>(Color.Hue - Offset),100,100};
+    vector <uint8_t> HsvMax = {static_cast<uint8_t>(Color.Hue + Offset),255,255};
     
+    //Make result image matrix with found color
+    inRange(FrameHSV, HsvMin, HsvMax, Threshold);
+    //Creating the image in white&black with area of needed color 
+    // imwrite( "FrameResult.jpg", Threshold );
+    return Threshold;
+}
+
+Rect detectBlob(Mat Threshold)
+{
+    Rect Rectangle;
+    vector<vector<Point>> Contours;
+    vector<Vec4i> Hierarchy;
+
+    findContours(Threshold, Contours, RETR_TREE, CHAIN_APPROX_SIMPLE);
+    
+    vector<RotatedRect> minRect(Contours.size());
+    for(size_t i = 0; i < Contours.size(); i++)
+            minRect[i] = minAreaRect(Contours[i]);
+    
+    for(size_t i = 0; i < Contours.size(); i++)
+    {
+        int area = minRect[i].size.height*minRect[i].size.width;
+        if(area > 10'000)
+        {
+            Rectangle = minRect[i].boundingRect();
+            //Uncomment if you want to see Rectangles on image
+            // Point2f rect_points[4];
+            // minRect[i].points( rect_points );
+            // for ( int j = 0; j < 4; j++ )
+            // {
+            //     line( Threshold, rect_points[j], rect_points[(j+1)%4], (0,0,255) );
+            // }
+        }
+    }
+    // imshow("Result", Threshold);
+    // waitKey();
+    return Rectangle;
+}
+
+int main()
+{
+    Mat Frame = imread("img_0.jpg");
+    Mat FrameHSV;
+    //converting the Frame to FrameHsv color model
+    cvtColor(Frame, FrameHSV, COLOR_BGR2HSV);
+    Point2i Destination(960,1280);
+    Mat Threshold = TakeThresholdOfBlob(FrameHSV, MagentaHSV);
+    Rect RectangleOfMagenta = detectBlob(Threshold);
+    //just for tests
+    // cout << RectangleOfMagenta << endl;
+
+    Threshold = TakeThresholdOfBlob(FrameHSV,BlueHSV);
+    Rect RectangleOfBlue = detectBlob(Threshold);
+    //just for tests
+    // cout << RectangleOfBlue << endl;
+
+    MassCenter MCBlue(RectangleOfBlue);
+    MassCenter MCMagenta(RectangleOfMagenta);
+    MassCenter MCAverage(MCBlue, MCMagenta);    
+    MovementCalculation MoveBot;
+    MoveBot.findAngle(MCBlue, MCMagenta, Destination);
+    MoveBot.findDistanceToDestination(MCAverage, Destination);
+    cout << "Angle: " << MoveBot.AngleInRadian << " Distance: " << MoveBot.DistanceInPixel << endl;
+
+    return 0;
 }
